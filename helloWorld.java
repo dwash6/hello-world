e76b5fc2fbace8c72dc6b3d8c65e13acc062d416
dwash6 <durand.washington@yahoo.com>
2022-03-24 16:44:53 -0400

    Revert "helloWorldEdit"
    This reverts commit 1b5502dbb3339a0b2642430bd3984f0fe08fbae8.

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,5 @@ public class helloWorld {
    public static void main(String[] args) {
       
       System.out.println("Hello world!");
-      System.out.println("Program modified, but able to revert to original version using GitHub Desktop");
    }
 }
